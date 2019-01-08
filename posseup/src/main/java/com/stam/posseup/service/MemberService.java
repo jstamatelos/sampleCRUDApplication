e1d3df06a7ad5ba754333520f096412b21bb2757
Jarryd Stamatelos <jarryd.stamatelos.com>
2019-01-07 20:45:18 -0500

    Removed logic from Controller Class into Service Class

---
+++ posseup/src/main/java/com/stam/posseup/service/MemberService.java
@@ -0,0 +1,52 @@
+package com.stam.posseup.service;
+
+import com.stam.posseup.entity.Member;
+import com.stam.posseup.exception.MemberExecption;
+import com.stam.posseup.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// Service Implementation class that abstracts the business logic away from the controller
+@Service
+public class MemberService {
+
+    // Autowiring in the Member Repository so we can use it with instantiating multiple times
+    @Autowired
+    private MemberRepository repository;
+
+    public List<Member> listAllMembers() {
+        return repository.findAll();
+
+    }
+
+    public Member createNewMember(Member newMember) {
+        return repository.save(newMember);
+
+    }
+
+    public Member retrieveMemberById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new MemberExecption(id));
+
+    }
+
+    public Member updateMember(Member newMember, Long id) {
+        return repository.findById(id).map(member -> {
+            member.setName(newMember.getName());
+            member.setPosition(newMember.getPosition());
+            return repository.save(member);
+            })
+            .orElseGet(() -> {
+                newMember.setId(id);
+                return repository.save(newMember);
+            });
+
+    }
+
+    public void deleteMember(Long id) {
+        repository.deleteById(id);
+    }
+
+
+}
